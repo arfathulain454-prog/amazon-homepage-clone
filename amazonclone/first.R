7956197142ab4ed7f07bf4f2d7cac422ef3ce879
arfathulain454-prog <arfathulain454@gmail.com>
2025-11-14 07:09:46 +0530

    Add files via upload

---
+++ amazonclone/first.R
@@ -0,0 +1,46 @@
+# Example Program in R
+# Calculate the average of numbers entered by the user
+
+# Take input from the user
+# numbers <- c(10, 20, 30, 40, 50)   # you can also use: scan() for manual input
+
+# # Display numbers
+# print("The numbers are:")
+# print(numbers)
+
+# # Calculate the average
+# average <- mean(numbers)
+
+# # Display result
+# cat("The average of given numbers is:", average, "\n")
+
+# z<-c("R","PYTHON","DBMS","HTML")
+# x<-z[-4]
+# print(x)
+
+# x<-c(1,4,5,8)
+# sort(x)
+# z<-sort(x,decreasing = TRUE)
+# print(z)
+
+# x<-c(10,20,30)
+# x<-NULL
+# print(z)
+# x<-c(1,2,100,3,4)
+# x[1:3]<-66
+# print(x)
+
+# x<-c("R", 1206, 66,"hour")
+# print(x)
+# x<-(10,11,15,20)
+# x<-list(10:20)
+# x<-unlist(10:20)
+# print(x)
+x<-list(10:20)
+print(x)
+a<-unlist(x)
+print(a)
+student_name<-c("arfath","mariyam")
+age<-c("19","20")
+home_town<-c("tmk","banglure")
+print(student_name[2])
